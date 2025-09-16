7ac41d5cd681c335915be32ca7fbdac95d450ee0
xgaslann <gorkem_aslan0@hotmail.com>
2025-09-17 01:04:46 +0300

    Enhance Kafka consumer with manual acknowledgment and improved configuration
    - Updated `OrderConsumerService` to include manual acknowledgment support and enhanced error handling.
    - Configured Kafka to use manual acknowledgment, disabled auto-commit, and set isolation level to "read_committed".
    - Added `OrderEvent` type deserialization settings in `KafkaConfiguration`.

--- src/main/java/com/xgaslan/stockservice/config/KafkaConfiguration.java
+++ src/main/java/com/xgaslan/stockservice/config/KafkaConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -24,6 +25,8 @@ public class KafkaConfiguration {
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactory){
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
         return factory;
     }
 
@@ -35,9 +38,16 @@ public class KafkaConfiguration {
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+        config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
         config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, OrderEvent.class.getCanonicalName());
+
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
         return new DefaultKafkaConsumerFactory<>(config);
     }
 }
--- src/main/java/com/xgaslan/stockservice/kafka/OrderConsumerService.java
+++ src/main/java/com/xgaslan/stockservice/kafka/OrderConsumerService.java
@@ -1,10 +1,12 @@
 package com.xgaslan.stockservice.kafka;
 
 import com.xgaslan.stockservice.constants.KafkaConstants;
+import com.xgaslan.stockservice.dto.OrderEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.kafka.support.Acknowledgment;
 
 @Slf4j
 @Service
@@ -17,7 +19,39 @@ public class OrderConsumerService {
             groupId = KafkaConstants.GROUP_ID,
             containerFactory = "kafkaListenerContainerFactory"
     )
-    public void consumeMessage(Object message) {
-        log.info("Consumed message: {}", message);
+    public void consumeMessage(OrderEvent event, Acknowledgment ack) {
+        try {
+            log.info("Processing order: {}", event.getOrder().getId());
+
+            // İdempotent check
+//            if (!isOrderProcessed(event.getOrder().getId())) {
+//                updateStock(event.getOrder());
+//                markOrderAsProcessed(event.getOrder().getId());
+//                log.info("Stock updated for order: {}", event.getOrder().getId());
+//            } else {
+//                log.info("Order already processed, skipping: {}", event.getOrder().getId());
+//            }
+
+            ack.acknowledge();
+
+        } catch (Exception e) {
+            log.error("Stock update failed for order: {}", event.getOrder().getId(), e);
+        }
     }
+
+//    private boolean isOrderProcessed(String orderId) {
+//        return processedOrderRepository.existsByOrderId(orderId);
+//    }
+//
+//    private void updateStock(Order order) {
+//        stockService.decreaseStock(order.getName(), order.getQuantity());
+//    }
+//
+//    private void markOrderAsProcessed(String orderId) {
+//        ProcessedOrder processedOrder = ProcessedOrder.builder()
+//                .orderId(orderId)
+//                .processedAt(LocalDateTime.now())
+//                .build();
+//        processedOrderRepository.save(processedOrder);
+//    }
 }
