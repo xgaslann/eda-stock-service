84a368567bcd9dd347670a6f60e44a196316d86b
xgaslann <gorkem_aslan0@hotmail.com>
2025-09-16 22:56:19 +0300

    xgaslan initial

---
+++ src/main/java/com/xgaslan/stockservice/EdaStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xgaslan.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaStockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdaStockServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgaslan/stockservice/EdaStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgaslan.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaStockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
